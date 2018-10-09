f07a201405164c20d97e51f01788b60a4870e260
Rian <rian.sj@gmail.com>
2018-10-09 22:31:26 +0700

    finishing service worker

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,8 +1,12 @@
+var staticCacheName = 'wittr-static-v5';
+
 self.addEventListener('install', function(event) {
+  // TODO: cache /skeleton rather than the root page
+
   event.waitUntil(
-    caches.open('wittr-static-v1').then(function(cache) {
+    caches.open(staticCacheName).then(function(cache) {
       return cache.addAll([
-        '/',
+        '/skeleton',
         'js/main.js',
         'css/main.css',
         'imgs/icon.png',
@@ -13,13 +17,42 @@ self.addEventListener('install', function(event) {
   );
 });
 
-self.addEventListener('fetch', function(event) {
-  // TODO: respond with an entry from the cache if there is one.
-  // If there isn't, fetch from the network.
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-        if (response) return response;
-        return fetch(event.request);
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('wittr-') &&
+                 cacheName != staticCacheName;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
     })
   );
+});
+
+self.addEventListener('fetch', function(event) {
+  // TODO: respond to requests for the root page with
+  // the page skeleton from the cache
+    var requestUrl = new URL(event.request.url);
+
+    if (requestUrl.origin === location.origin) {
+        if (requestUrl.pathname === "/") {
+            event.respondWith(caches.match('/skeleton'));
+            return;
+        }
+    }
+
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('message', function(event) {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
 });
